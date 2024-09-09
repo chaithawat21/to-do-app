82ef3c27220d5c80595eeb8425e76d9f505d0870
chai <chaithawat.contact@gmail.com>
2024-09-09 14:50:38 +0700

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Chaithawat Pinsuwan on 9/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
